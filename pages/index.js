3257a02f784e781614ade429b30d87d47aa3a642
brayokenya <kiirubrian21@gmail.com>
2022-07-07 21:08:01 +0300

    connect with sanity to pull data from db

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,19 @@ import React from "react";
 import { Product, FooterBanner, HeroBanner } from '../components';
 
 
-const Home = () => {
-    return ( 
-        <>
-            HeroBanner 
+const Home = () => (
+    
+    <div>
+        <HeroBanner/>
+             
             <div className='products-heading'>
             <h2> Best Selling Products </h2> <p> Speakers of many variations </p > 
             </div> 
             <div className='products-container' > {
                 ["Product 1", "Product 2"].map((product) => product) } 
                 </div> 
-        </>
+        <FooterBanner/>
+    </div>
     );
-};
 
 export default Home;
