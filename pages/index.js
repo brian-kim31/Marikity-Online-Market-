dfee6bd0cb480294f40617bedb1e61f6ac9bc9fe
brayokenya <kiirubrian21@gmail.com>
2022-07-07 22:50:30 +0300

    consumed db values on index page

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,36 @@
 import React from "react";
 import { Product, FooterBanner, HeroBanner } from '../components';
+import { client } from "../lib/client";
 
 
-const Home = () => (
+const Home = ({ products, bannerData }) => (
     
     <div>
-        <HeroBanner/>
+        <HeroBanner heroBanner = {bannerData.length && bannerData [0]}/>
+        {console.log(bannerData)}
              
             <div className='products-heading'>
             <h2> Best Selling Products </h2> <p> Speakers of many variations </p > 
             </div> 
-            <div className='products-container' > {
-                ["Product 1", "Product 2"].map((product) => product) } 
+            <div className='products-container' > 
+            { products?.map((product) => product.name) 
+            
+            } 
                 </div> 
         <FooterBanner/>
     </div>
     );
+
+    export const getServerSideProps = async () => {
+        const query = '*[_type == "prpduct"]';
+        const products = await client.fetch(query);
+
+        const bannerQuery = '*[_type == "banner"]';
+        const bannerData = await client.fetch(bannerQuery);
+
+        return {
+            props: { products, bannerData }
+        }
+    }
 
 export default Home;
