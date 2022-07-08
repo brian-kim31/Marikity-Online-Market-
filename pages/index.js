a16e9920db422a3a07ca9cced07eca214b9e0d7b
brayokenya <kiirubrian21@gmail.com>
2022-07-08 18:05:41 +0300

    show best seller products

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,34 @@
-import React from "react";
-import { Product, FooterBanner, HeroBanner } from '../components';
-import { client } from "../lib/client";
+import React from 'react';
 
+import { client } from '../lib/client';
+import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = ({ products, bannerData }) => (
-    
-    <div>
-        <HeroBanner heroBanner = {bannerData.length && bannerData [0]}/>
-        {console.log(bannerData)}
-             
-            <div className='products-heading'>
-            <h2> Best Selling Products </h2> <p> Speakers of many variations </p > 
-            </div> 
-            <div className='products-container' > 
-            { products?.map((product) => product.name) 
-            
-            } 
-                </div> 
-        <FooterBanner/>
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
     </div>
-    );
 
-    export const getServerSideProps = async () => {
-        const query = '*[_type == "prpduct"]';
-        const products = await client.fetch(query);
+    <div className="products-container">
+      {products?.map((product) => <Product key={product._id} product={product} />)}
+    </div>
 
-        const bannerQuery = '*[_type == "banner"]';
-        const bannerData = await client.fetch(bannerQuery);
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+);
 
-        return {
-            props: { products, bannerData }
-        }
-    }
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+}
 
 export default Home;
