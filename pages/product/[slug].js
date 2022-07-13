977f8c511702938d94a0da505915962bf3cdcdb0
brayokenya <kiirubrian21@gmail.com>
2022-07-13 13:18:03 +0300

    ft-product detail image

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,11 +1,62 @@
-//file based routing in Next js as opposed to React router Lib in React
+//file based routing in Next js as opposed to React router Lib
 
 import React from "react";
+import { client, urlFor } from '../../lib/client';
+ 
 
-const ProductDetails = () => {
+const ProductDetails = ({ product, products }) => {
+    const { image, name, details, price } = product;
     return(
-        <div>Product</div>
+        <div>
+            <div
+            className="product-detail-container">
+                <div>
+                    <div className="image-container">
+                    <img src={urlFor(image && image[0])} className="product-detail-image" />
+                    </div>
+                    
+                </div>
+
+            </div>
+
+
+        </div>
     )
 }
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+    slug {
+      current
+    }
+  }
+  `;
+  const products = await client.fetch(query);
+
+  const paths = products.map((product) => ({
+    params: { 
+      slug: product.slug.current
+    }
+  }));
+
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+}
+//used if the data required is available at build time at users request
+//modified line 29 to template string by use of back tics ``
+export const getStaticProps = async ({ params: { slug }}) => {
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+    const productsQuery = '*[_type == "product"]'
+    
+    const product = await client.fetch(query);
+    const products = await client.fetch(productsQuery);
+  
+    console.log(product);
+  
+    return {
+      props: { products, product }
+    }
+  }
 
 export default ProductDetails
