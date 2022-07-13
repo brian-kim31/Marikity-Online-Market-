485376ca95de7897b99d606421b2afc737ba475d
brayokenya <kiirubrian21@gmail.com>
2022-07-13 18:16:00 +0300

    ft-you may aso like product marquee

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,6 +3,8 @@
 import React from "react";
 import { client, urlFor } from '../../lib/client';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import { Product } from '../../components';
+
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
@@ -58,9 +60,18 @@ const ProductDetails = ({ product, products }) => {
                 </div>
 
             </div>
-
-
+                <div className="maylike-products-wrapper">
+                    <h2>You may also like</h2>
+                    <div className="marquee">
+                        <div className="maylike-products-container track">
+                        {products.map((item) => (
+                            <Product key={item._id} product={item} />
+                        ))}
+                    </div>
+                </div>
         </div>
+    </div>
+ 
         
     )
 }
