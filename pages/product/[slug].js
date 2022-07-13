9190f290f88192c760e51d3bfeb981785e3eaac0
brayokenya <kiirubrian21@gmail.com>
2022-07-13 12:44:11 +0300

    ft-prodcut detail

---
+++ pages/product/[slug].js
@@ -0,0 +1,11 @@
+//file based routing in Next js as opposed to React router Lib in React
+
+import React from "react";
+
+const ProductDetails = () => {
+    return(
+        <div>Product</div>
+    )
+}
+
+export default ProductDetails
