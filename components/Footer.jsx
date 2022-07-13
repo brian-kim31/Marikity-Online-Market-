16543f4af99dab58e928ae513fffd2fe1966aafd
brayokenya <kiirubrian21@gmail.com>
2022-07-13 12:25:43 +0300

    added footer and banner

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter, AiOutlineGithub} from 'react-icons/a
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 JSM Headphones All rights reserverd</p>
+      <p>2022 Marikity Headphones All rights reserverd</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">JSM Headphones</Link>
+        <Link href="/">Marikity Headphones</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick=''>
