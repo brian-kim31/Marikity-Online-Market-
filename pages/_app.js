2f0bc35c836ffa3ff9f03c9beaef586e599b7510
brayokenya <kiirubrian21@gmail.com>
2022-07-13 12:22:11 +0300

    added footer and banner

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
-import "../styles/globals.css";
 import React from "react";
+import { Toaster } from "react-hot-toast";
+
+import { Layout } from "../components";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps }
-    />;
+  return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+  );
 }
 
 export default MyApp;
