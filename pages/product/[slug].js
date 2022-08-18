12f3a8923a4fddfdd4d671ff6c84bfef71729c26
brayokenya <kiirubrian21@gmail.com>
2022-08-18 16:44:13 +0300

    added image browsing on cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,6 @@
 //file based routing in Next js as opposed to React router Lib
 
-import React from "react";
+import React, {useState} from "react";
 import { client, urlFor } from '../../lib/client';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { Product } from '../../components';
@@ -8,24 +8,26 @@ import { Product } from '../../components';
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
+    const [index, setIndex] = useState(0);
     return(
         <div>
             <div
                 className="product-detail-container">
                     <div>
                         <div className="image-container">
-                        <img src={urlFor(image && image[0])} className="product-detail-image" />
+                        <img src={urlFor(image && image[index])} className="product-detail-image" />
                         </div>
-                        {/* <div className="small-images-container">
+                        <div className="small-images-container">
                             {image?.map((item, i) => (
                             <img 
                                 
                                 src={urlFor(item)}
-                                className=""
-                                onMouseEnter=""
+                                className={i === index ? 'small-image selected-image':
+                            'small-image'}
+                                onMouseEnter={() => setIndex(i)}
                             />
                             ))}
-                        </div> */}
+                        </div> 
                         
                     </div>
                     <div className="product-detail-desc">
